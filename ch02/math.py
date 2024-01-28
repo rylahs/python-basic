3d2bf32ded4d9f4d783b909dce09b5b2e02b255d
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-28 18:44:24 +0900

    Create math.py

---
+++ ch02/math.py
@@ -0,0 +1,17 @@
+"""
+This module provides functions to perform mathematical operations.
+Includes functions to perform addition, subtraction, multiplication, and division.
+"""
+from math import floor, ceil, sqrt
+
+print(abs(-5)) # 5
+print(pow(4, 2)) # 4^2 = 4 * 4 = 16
+print(max(5, 12)) # 12
+print(min(5, 12)) # 5
+print(round(3.14)) # 3
+print(round(4.99)) # 5
+
+
+print(floor(4.99)) # 내림 4
+print(ceil(3.14)) # 올림 4
+print(sqrt(16)) # 제곱근 4
