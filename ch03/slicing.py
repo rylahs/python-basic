3adeac6c3bd2876261ac587a7bf01b3239b8f8b7
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-29 22:50:29 +0900

    Create slicing.py

---
+++ ch03/slicing.py
@@ -0,0 +1,10 @@
+jumin = "990103-1234567"
+
+print("성별 : " + jumin[7])
+print("연 : " + jumin[0:2]) # 0 ~ 2 미만
+print("월 : " + jumin[2:4])
+print("일 : " + jumin[4:6])
+print("생년월일 : " + jumin[:6])
+print("뒤 7자리 : " + jumin[7:])
+print("뒤 7자리(로꾸꺼) : " + jumin[-7:])
+
