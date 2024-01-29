d7ba0bf54f28a749f0ac0dd94ccf146300fb4ae9
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-29 22:53:30 +0900

    Create strfunc.py

---
+++ ch03/strfunc.py
@@ -0,0 +1,20 @@
+python = "Python is Amazing"
+
+print(python.lower())
+print(python.upper())
+print(python[0].isupper())
+print(len(python))
+print(python.replace("Python", "Java"))
+
+index = python.index("n")
+print(index)
+
+index = python.index("n", index + 1)
+print(index)
+
+print(python.find("n"))
+print(python.find("Java"))
+# print(python.index("Java")) # Error
+print("hi")
+
+print(python.count("n"))
