ebda3bcc6c5a4b84dc4f095e56b02f7504daa3d2
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-29 23:42:52 +0900

    Create escape.py

---
+++ ch03/escape.py
@@ -0,0 +1,19 @@
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표
+print("저는 \"나도코딩\"입니다.")
+print('저는 "나도코딩"입니다.')
+print("저는 \'나도코딩\'입니다.")
+
+# \\ : 문장 내에서 \
+print("C:\\Users\\Nadocoding\\Desktop\\PythonWorkspace")
+
+# \r : 커서의 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스 (한글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
